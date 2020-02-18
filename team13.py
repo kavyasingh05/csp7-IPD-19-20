b3bbdaec7594da8758719f04a58c4774c241d3c0
kavyasingh05 <singhkavya05@gmail.com>
2020-02-18 23:20:16 +0000

    I added my strategy into the Team 13 file. Our team is The Semi C.

--- team13.py
+++ team13.py
@@ -28,3 +28,21 @@ def move(my_history, their_history, my_score, their_score):
     
     return 'c'
 
+team_name = 'The Semi C(ompetent Programmers)' # Only 10 chars displayed.
+strategy_name = 'Two Wrongs Might Make a Right'
+strategy_description = '''Betray until the opponent betrays twice in a row, then collude until the opponent betrays again'''
+    
+def move(my_history, their_history, my_score, their_score):
+    first_turn = True
+    second_turn = True
+    if first_turn == True:
+      first_turn = False
+      return 'b'
+    elif second_turn == True:
+      second_turn = False
+      return 'b'
+    while their_history[-1] != "b" and their_history[-2] != b:
+      return 'b'
+    else:
+      while their_history[-1] != "b":
+        return 'c'
